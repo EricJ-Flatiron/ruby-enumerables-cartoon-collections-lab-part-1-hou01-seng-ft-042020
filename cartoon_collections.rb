544a1f5580a6c8e28dbd725c50dce6c7feb0a032
Eric Johnson <johnson.n.eric@gmail.com>
2020-04-16 00:42:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   array.each_with_index do |name , index|
-    p index.to_string + ". " + name
+    p index.to_s + ". " + name
   end
   
   
